4bbd9e5dd28cd885bbb665779d8d6ffe5443858f
Дмитрий Cry <cry.int@gmail.com>
2023-09-17 20:27:56 +0200

    first test of mqtt and average temperature value

--- 10k.ino
+++ 10k.ino
@@ -1,4 +1,5 @@
 #include <WiFi.h>
+#include <PubSubClient.h>
 
 #include "config/config.h"
 
@@ -12,6 +13,18 @@ BEEPER Sound(BEEPER_PIN);
 THERMISTOR Thermistor(THERMISTOR_PIN);
 DISPLAY_I2C Display;
 
+#define mqtt_server "192.168.77.242"
+#define mqtt_user ""
+#define mqtt_password ""
+
+#define temperature_topic "sensor/outdoor_water_temperature"
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+int Tick = 1;
+double TemperatureArray[10];
+
 void setup()
 {
   Serial.begin(9600);
@@ -24,36 +37,75 @@ void setup()
   // connect to WiFi
   Serial.printf("Connecting to %s ", WIFI_SSID);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
-  }
-  // ====
 
   Sound.beep();
   OnBoardLed.blinkFast(3);
 }
 
+float average (double * array, int len)
+{
+  long sum = 0L;
+  for (int i = 0; i < len; i++) {
+    sum += array[i];
+  }
+  return ((float)sum) / len;
+}
+
 void loop()
 {
-  Serial.println("Tick!");
+  Serial.println(String("Tick: ") + Tick);
 
   double temperature = Thermistor.getTemperature();
-  Serial.println(temperature);
+  Serial.println(String("Received temperature: ") + temperature);
+
+  TemperatureArray[Tick - 1] = temperature;
 
   String temperatureString = String(temperature, 2);
+
+  double temperatureAverage = average(TemperatureArray, 10);
+  Serial.println(String("Average temperature: ") + temperatureAverage);
+
+  String temperatureStringAverage = String(temperatureAverage, 2);
 
   Display.clear();
 
   if (WiFi.status() == WL_CONNECTED) {
-    Display.drawImage(111, 0, ICON_WIFI_HOTSPOT, ICON_WIFI_HOTSPOT_WIDTH, ICON_WIFI_HOTSPOT_HEIGHT);
+    Display.drawImage(90, 0, ICON_WIFI_HOTSPOT, ICON_WIFI_HOTSPOT_WIDTH, ICON_WIFI_HOTSPOT_HEIGHT);
+
+    if (client.connected() == false) {
+      client.setServer(mqtt_server, 1883);
+      if (client.connect("ESP32WTClient", mqtt_user, mqtt_password)) {
+        Serial.println("connected");
+      } else {
+        Serial.print("failed, rc=");
+        Serial.println(client.state());
+      }
+    }
+    else {
+      Display.drawImage(111, 0, ICON_WIFI_HOTSPOT, ICON_WIFI_HOTSPOT_WIDTH, ICON_WIFI_HOTSPOT_HEIGHT);
+
+      if (Tick >= 10) {
+        client.publish(temperature_topic, String(temperatureStringAverage).c_str(), true);
+        Serial.println(String("Publish temperature: ") + temperatureStringAverage);
+        Tick = 0;
+      }
+    }
   }
 
-  Display.println(20, 30, 3, temperatureString);
+  Display.println(0, 5, 1, temperatureString);
+  Display.println(20, 30, 3, temperatureStringAverage);
 
   Display.show();
 
   OnBoardLed.blink(100);
+
+  for(int i = 0; i < 10; i++) {
+    Serial.print(TemperatureArray[i]);
+    Serial.print(" | ");
+  }
+  Serial.println("");
+
+  Tick++;
 
   delay(1000);
 }
--- config/config.h
+++ config/config.h
@@ -9,7 +9,7 @@
 #define SCREEN_WIDTH 128 // OLED width,  in pixels
 #define SCREEN_HEIGHT 64 // OLED height, in pixels
 
-double BOARD_VOLTAGE = 3.26; // Voltage from divider
+double BOARD_VOLTAGE = 3.3; // Voltage from divider
 double DIVIDER_RESISTOR_VALUES = 10000; // divider resistor value
 
 double REFERENCE_TEMPERATURE_IN_KELVIN_FOR_25 = 298.15; // Temperature in Kelvin for 25 degree Celsius
